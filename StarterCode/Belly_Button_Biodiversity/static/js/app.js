9b55a0410a557afc201c3ffbe51253368004039a
abdulib <abdulrahmon.ibrahim@yahoo.com>
2019-05-30 12:49:57 -0500

    Update app.js

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -4,27 +4,51 @@ function buildMetadata(sample) {
 
   // Use `d3.json` to fetch the metadata for a sample
     // Use d3 to select the panel with id of `#sample-metadata`
+    d3.json(`/metadata/${sample}`).then(function(data){
+      console.log(data)
+      
+      var metadatapanel = d3.select("#sample-metadata")
 
-    // Use `.html("") to clear any existing metadata
+      // Use `.html("") to clear any existing metadata
+
+      metadatapanel.html("")
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
 
+    Object.Entries(data).forEach(([key, value]) =>{
+      metadatapanel.append("p")
+      .text(`${key}:${value}`)
+
+    });
+
+    });
+
+
+
     // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+    buildGauge(data.WFREQ);
 }
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
+  d3.json(/samples/`${sample}`).then(function(data){
 
     // @TODO: Build a Bubble Chart using the sample data
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-}
+  })
+
+
+
+
+  })
+
+
 
 function init() {
   // Grab a reference to the dropdown select element
